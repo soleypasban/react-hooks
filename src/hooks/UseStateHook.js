0a47887d1a19a66cc33f664306500c9cbb8468f5
Soley Pasban <pasban110@gmail.com>
2019-09-20 12:24:22 +0300

    refactoring

--- src/hooks/UseStateHook.js
+++ src/hooks/UseStateHook.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 
-
-const FormExample = () => {
+const UseStateHook = () => {
     const [values, SetValues] = useState({
-        username: ''
+        username: '',
+        email: ''
     })
+
+    const onChange = e => SetValues({ ...values, [e.target.name]: e.target.value })
 
     const onSubmit = () => {
         alert(values.username)
@@ -12,23 +14,14 @@ const FormExample = () => {
 
     return (
         <div className="App">
-            <input type='text' name='username' value={values.username}
-                onChange={e => SetValues({ username: e.target.value })}
-            />
+            <input type='text' name='email' placeholder='email' value={values.email} onChange={onChange} />
+            <input type='text' name='username' placeholder='username' value={values.username} onChange={onChange} />
             <button onClick={onSubmit}> Submit </button>
+            <pre>
+                {JSON.stringify(values, null, 2)}
+            </pre>
         </div>
     );
 }
 
-const CounterExample = () => {
-    const [counter, setCounter] = useState(0)
-    return (
-        <div className="App">
-            <button onClick={() => setCounter(c => c - 1)}> Dec </button>
-            <span className='with-padding-01'> {counter} </span>
-            <button onClick={() => setCounter(c => c + 1)}> Inc </button>
-        </div>
-    );
-}
-
-export const UseStateHook = FormExample
+export { UseStateHook }
